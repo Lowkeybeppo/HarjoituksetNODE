73a643c81aa9f0ee385aaea3d567ed0c391b54ed
Lowkeybeppo <rekopuoskari@gmail.com>
2025-11-25 13:35:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const sanonnat = require('./sanonnat')
 const lodash = require('lodash')
+const fs = require('fs')
 
 //initialisoidaan express-applikaatio (expressjs.com)
 const app = express()
@@ -21,20 +22,60 @@ app.set('view engine','ejs') //määritetään app käyttämään ejs template e
 //     return arr;
 // }
 
+app.use(express.urlencoded({extended: false}))  //Aina kun koodissa on app.use, käytetään valmiiksi sisäänrakennettua middlewarea
+
+
+
 //ROUTES AND ENDPOINTS BELOW-----------------------------------------------------------
 app.get('/', (req, res) => {
-    const userAgent = req.headers['user-agent']
-    console.log(userAgent)
+    // const userAgent = req.headers['user-agent'] //userAgent kertoo mitä selainta käyttäjä käyttää ja req.headers pyytää headerista nämä tiedot
+    // console.log(userAgent)
     
-    const userLocale = req.headers['accept-language']
-    console.log(userLocale)
-
+    // const userLocale = req.headers['accept-language'] //userLocale kertoo mitä kieltä käyttäjä käyttää ja req.headers pyytää headerista nämä tiedot
+    // console.log(userLocale)
+    
+    res.render('index',{sanonta:lodash.sample(sanonnat.sanonnat)})  //Hakee index.ejs tiedoston, sekä sanonta tiedoston, ja käyttää sanonta tiedostoon lodash samplea
+    })
   //res.send('Hello World!')
+
+
+app.get('/login', (req, res) => {
+    res.render('login')
+    })
+
+app.post('/login', (req, res) => {
+    res.render('welcome', {kayttajanimi:req.body.username})  
+    })
+
+app.get('/saveuser', (req, res) => {
+    res.render('saveuser')
+
+})
+
+app.post('/saveuser', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    if (!username) {
+        return res.status(400).send('Missing username')
+    }
+
+    // Kirjaa käyttäjänimi tiedostoon kayttajat.txt
+    fs.appendFile('kayttajat.txt', username + '\n' + password + '\n' + '\n', (err) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).send('Error saving username')
+        }
+
+        // uudelleenohjaa käyttäjän etusivulle
+        res.redirect('/')
+    })
+
+})
+
+
 
 //  const randomi = shuffleArray(sanonnat.sanonnat)
  // res.render('index',{randomi}) //hakee index.ejs tiedoston views kansiosta.
- res.render('index',{sanonta:lodash.sample(sanonnat.sanonnat)})
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
